28857ccacf2a964a2feee9842fbf1a66b8f55ceb
Maksym Shyshko <maxim.shishko95@gmail.com>
2021-06-20 02:34:42 +0300

    updated api and render functions
    -turned on preloader
    -updated CSS for movie cards to make them animated
    -turned on test-pagination
    -updated api and render services

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import './sass/main.scss';
 import { getGenres } from "./js-modules/api-service.js";
-import {renderTrendingFilms} from "./js-modules/render-service.js"
+import { renderTrendingFilms, renderTrendingFilmsByPageNum } from "./js-modules/render-service.js";
+import variables from './js-modules/variables.js';
 import './js-modules/popap';
+import Pages from "./js-modules/pagination.js";
 
-const filmGrid = document.querySelector('.film__grid');
+
+// Рендер панели с пагинацией и первой страницы с трендовыми фильмами
+const pagination = new Pages();
+const initMainMarkup = async function () {
+    const totalPages = await renderTrendingFilms(variables.filmGrid, variables.preloader);
+    pagination.moveToPage(1, totalPages);
+}
+// *********************************************************************
 
 getGenres();
-renderTrendingFilms(filmGrid);
+initMainMarkup();
+
+
+
 
 
 
--- src/js-modules/render-service.js
+++ src/js-modules/render-service.js
@@ -1,7 +1,17 @@
 import trendingFilmsMarkupFc from "../hbs-templates/trending-films.hbs";
-import { getTrendingFilms } from "./api-service.js";
+import { getTrendingFilms, getTrendingFilmsByPageNum } from "./api-service.js";
 
-export const renderTrendingFilms = async function (container) {
-    const movies = await getTrendingFilms();
-    container.insertAdjacentHTML('beforeend', trendingFilmsMarkupFc(movies));
+export const renderTrendingFilms = async function (container, preloader) {
+    const movies = await getTrendingFilms(preloader);
+    container.insertAdjacentHTML('beforeend', trendingFilmsMarkupFc(movies.updatedFilmData));
+    preloader.classList.add('preloader-hidden');
+    return movies.totalPages;
+};
+
+export const renderTrendingFilmsByPageNum = async function (container, preloader, page) {
+    const movies = await getTrendingFilmsByPageNum(preloader, page);
+    container.innerHTML = '';
+    container.insertAdjacentHTML('beforeend', trendingFilmsMarkupFc(movies.updatedFilmData));
+    preloader.classList.add('preloader-hidden');
+    return movies.totalPages;
 };
