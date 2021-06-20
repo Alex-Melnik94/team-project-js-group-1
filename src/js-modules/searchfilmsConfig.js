212062fb6e4e1ed6fd48328d5084522c99dd952b
Alexander Krupka <bamaboy483@gmail.com>
2021-06-20 17:07:57 +0300

    +

--- src/js-modules/searchfilmsConfig.js
+++ src/js-modules/searchfilmsConfig.js
@@ -1,11 +1,12 @@
 import apiService from './apiService.js';
 import { appendGalleryMarkup } from "./render-service.js"
 
+const preloader = document.querySelector('.preloader');
 const searchInput = document.querySelector('.header__search')
 
 function onSearch(e){    
   e.preventDefault();                
-    
+  preloader.classList.remove('preloader-hidden');
   apiService.searchQuery = e.currentTarget.elements.query.value.trim();
     if (apiService.searchQuery === '') {
     return;
@@ -13,9 +14,11 @@ function onSearch(e){
   e.currentTarget.elements.query.value = '';
   
   apiService.fetchMovies().then(appendGalleryMarkup)
+    
     .catch(error => {
       notifications.errorRequest();
     });
+  
     
 }
 
