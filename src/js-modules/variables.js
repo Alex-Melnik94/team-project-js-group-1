d1db8d5605fb3f344d75318a5ccff662a6541c06
Maksym Shyshko <maxim.shishko95@gmail.com>
2021-06-21 20:44:19 +0300

    edited modal rendering feature
    - created api function on modal click
    - edited render function based on api
    - corrected hbs template for rendering modal window

--- src/js-modules/variables.js
+++ src/js-modules/variables.js
@@ -2,4 +2,7 @@ export default {
     filmGrid: document.querySelector('.film__grid'),
     preloader: document.querySelector('.preloader'),
     searchError: document.querySelector('.search-error'),
+    btnCloseModal: document.querySelector('[data-action="close-popap"]'),
+    modalContentBox: document.querySelector('[data-film="popap"]'),
+    backdropBox: document.querySelector('.popap'),
 }
